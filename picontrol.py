dc2da385b953107da9b3c1aada40d4c5d1204507
waterver <1484896653@qq.com>
2021-02-02 22:47:02 +0800

    change the turn for three to one

--- picontrol.py
+++ picontrol.py
@@ -2,31 +2,40 @@ from RPi import GPIO
 import time
 
 class MOTOR():
-    def __init__(self,num=0):
+    def __init__(self,num=0,dirm=None,pwm=None,fre=1000):
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         self.num=num
-
+        self.dirm=dirm
+        self.pwmchannel=pwm
+        self.fre=fre
+        self.steup(self.dirm,self.pwmchannel,self.fre)
     
-    def steup(self,dirm=40,pwm=38,fre=1000):
+    def steup(self,dirm,pwm,fre):
         GPIO.setup(dirm,GPIO.OUT)
         GPIO.setup(pwm,GPIO.OUT)
-        self.dirm=dirm
-        self.pwm_channel=pwm
-        self.p=GPIO.PWM(pwm,fre)
-        self.fre=fre
+        self.pwm=GPIO.PWM(pwm,fre)
 
     def change(self,num):
         t=self.num-num
-        if t>0:
+        if t>0 and t<= 2:
             GPIO.output(self.dirm,1)
-        else:
+        elif t<0 and t>=-2:
             GPIO.output(self.dirm,0)
             t=-t
+        elif t==3:
+            GPIO.output(self.dirm,0)
+            t-=2
+        else:
+            GPIO.output(self.dirm,1)
+            t+=2
             
         step=102.6 *t/1.8*16/self.fre
-        self.p.start(50)
+        self.pwm.start(50)
         time.sleep(step)
-        self.p.stop()
+        self.pwm.stop()
         self.num=num
 
+if __name__=="__main__":
+    stepmoter=MOTOR(0,)
+    stepmoter.
